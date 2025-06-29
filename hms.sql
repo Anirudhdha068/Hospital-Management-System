709cdeb638409a309a06bbeecf27b6aecda0b6cf
Anirudhdha068 <anirudhdha068@gmail.com>
2025-06-29 11:53:54 +0530

    Update hms.sql

--- hms.sql
+++ hms.sql
@@ -40,9 +40,7 @@ CREATE TABLE `admin_users` (
 --
 
 INSERT INTO `admin_users` (`id`, `name`, `email`, `password`, `created_at`) VALUES
-(1, 'Anirudhdha', 'a@gmail.com', '1234567890', '2025-03-23 12:35:57'),
-(2, 'Prashant', 'p@gmail.com', '1234567890', '2025-03-23 12:35:57'),
-(3, 'Aadarsh', 'aa@gmail.com', '1234567890', '2025-03-23 12:35:57');
+(1, 'admin', 'admin@gmail.com', 'admin', '2025-03-23 12:35:57');
 
 -- --------------------------------------------------------
 
@@ -68,10 +66,8 @@ CREATE TABLE `appointments` (
 --
 
 INSERT INTO `appointments` (`id`, `name`, `email`, `phone`, `date`, `timeslot`, `department`, `message`, `created_at`, `user_id`) VALUES
-(1, 'ani', 'a@gmail.com', '1234567890', '2025-05-24', '9:00 AM - 11:00 AM', 'Neurology', 'hello sir', '2025-05-01 04:58:46', 1),
-(2, 'aaa', 'a@gmail.com', '1234567890', '2025-05-29', '12:00 PM - 2:00 PM', 'Neurology', '', '2025-05-01 05:02:27', 1),
-(3, 'asdfghj', 'a@gmail.cjo', '1234567890', '2025-05-29', '9:00 AM - 11:00 AM', 'Neurology', '', '2025-05-01 05:03:47', 1),
-(4, 'asdfghj', 'a@gmail.cjo', '1234567890', '2025-05-29', '9:00 AM - 11:00 AM', 'Neurology', '', '2025-05-01 05:04:44', 1);
+(1, 'aaa', 'a@gmail.com', '1234567890', '2025-05-24', '9:00 AM - 11:00 AM', 'Neurology', 'hello sir', '2025-05-01 04:58:46', 1),
+(2, 'bbb', 'b@gmail.com', '1234567890', '2025-05-29', '12:00 PM - 2:00 PM', 'Neurology', '', '2025-05-01 05:02:27', 1);
 
 -- --------------------------------------------------------
 
@@ -93,30 +89,9 @@ CREATE TABLE `billing` (
 --
 
 INSERT INTO `billing` (`id`, `patient_id`, `services`, `amount`, `created_at`, `razorpay_payment_id`) VALUES
-(3, 1, 'General Consultation', 5000000.00, '2025-04-07 11:29:48', NULL),
-(4, 1, 'General Consultation', 5000000.00, '2025-04-07 11:33:47', NULL),
-(5, 1, 'General Consultation', 5000000.00, '2025-04-07 11:35:01', NULL),
-(6, 2, 'General Consultation, Dental Treatment, Lab Test', 84003003.00, '2025-04-07 11:35:48', NULL),
-(7, 4, 'General Consultation, Surgery, Dental Treatment, Lab Test, X-Ray, Physiotherapy, Emergency Services', 99999999.99, '2025-04-07 21:47:43', NULL),
-(8, 2, 'General Consultation, X-Ray', 200.00, '2025-04-30 21:26:34', NULL),
-(9, 2, 'Surgery', 2500.00, '2025-04-30 22:46:50', NULL),
-(10, 2, 'Surgery', 2500.00, '2025-04-30 22:46:57', NULL),
-(11, 1, 'General Consultation', 300.00, '2025-04-30 22:58:53', NULL),
-(12, 1, 'General Consultation', 300.00, '2025-04-30 23:43:42', NULL),
-(13, 2, 'General Consultation', 300.00, '2025-04-30 23:44:33', NULL),
-(14, 2, 'General Consultation', 300.00, '2025-04-30 23:53:49', NULL),
-(15, 2, 'General Consultation', 300.00, '2025-04-30 23:54:48', NULL),
-(16, 2, 'General Consultation', 300.00, '2025-04-30 23:58:57', NULL),
-(17, 2, 'General Consultation', 300.00, '2025-05-01 00:00:08', NULL),
-(18, 1, 'General Consultation', 300.00, '2025-05-01 00:04:05', NULL),
-(19, 1, 'General Consultation', 300.00, '2025-05-01 00:04:11', NULL),
-(20, 1, 'General Consultation', 300.00, '2025-05-01 00:04:18', NULL),
-(21, 1, 'General Consultation', 300.00, '2025-05-01 00:04:24', NULL),
-(22, 1, 'General Consultation', 300.00, '2025-05-01 00:21:25', NULL),
-(23, 1, 'General Consultation', 300.00, '2025-05-01 00:56:28', NULL),
-(24, 1, 'General Consultation', 300.00, '2025-05-01 01:13:50', NULL),
-(25, 1, 'General Consultation', 300.00, '2025-05-01 01:14:08', NULL),
-(26, 1, 'General Consultation', 300.00, '2025-05-01 08:15:17', 'pay_QPVMV3RGqpI1u5');
+(1, 1, 'General Consultation', 5000000.00, '2025-04-07 11:29:48', NULL),
+(2, 1, 'General Consultation', 5000000.00, '2025-04-07 11:33:47', NULL),
+(3, 1, 'General Consultation', 5000000.00, '2025-04-07 11:35:01', NULL);
 
 -- --------------------------------------------------------
 
@@ -139,8 +114,8 @@ CREATE TABLE `staff` (
 --
 
 INSERT INTO `staff` (`id`, `name`, `email`, `phone`, `role`, `created_at`, `status`) VALUES
-(2, 'ram', 'a@gmail.com', NULL, 'Nurse', '2025-04-07 05:34:33', 'active'),
-(3, 'raam', 'a@gmail.com', NULL, 'Receptionist', '2025-04-07 05:39:28', 'active');
+(1, 'aa', 'aa@gmail.com', NULL, 'Nurse', '2025-04-07 05:34:33', 'active'),
+(2, 'bb', 'bb@gmail.com', NULL, 'Receptionist', '2025-04-07 05:39:28', 'active');
 
 -- --------------------------------------------------------
 
@@ -167,10 +142,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `first_name`, `middle_name`, `last_name`, `gender`, `age`, `blood_group`, `mobile_number`, `email`, `password`, `created_at`) VALUES
-(1, 'anirudhdha0', 'vipulbhai', 'poriya', 'Male', 19, 'B+', '1234567890', 'a@gmail.com', '$2y$10$1ZJi.oAHJ8mBnNU6ZMxE/.F5PJ7c9yz764sFP4UQ4IDU6x73EerR6', '2025-03-23 08:19:38'),
-(2, 'adarsh', 'zx', 'zxc', 'Male', 1, 'AB+', '1234567891', 'aa@gmail.com', '$2y$10$10NCaA1cvZTguuSIfzYQpOsP6qxFiuOvbPmBY.MHlcHI8yO5FdOSK', '2025-03-24 04:41:49'),
-(3, 'aaa', 'aaa', 'aaa', 'Male', 14, 'A-', '1234567999', 'aaa@gmail.com', '$2y$10$sF0i6sX2wTInabKLsmlUzOS9bcfxACCOmqdwFfNCPXOVHchTBFbFm', '2025-04-07 03:34:16'),
-(4, 'mayank', 'mahendrabhai', 'chauhan', 'Male', 19, 'O+', '9773433347', 'mayankchauhan4123@gmail.com', '$2y$10$iaHYmMzAWjsSHOz8xUeCfezp6wZYJDMpb47KbTjfZka4Z7T1UKNIi', '2025-04-07 16:14:52');
+(1, 'aaa', 'aaa', 'aaa', 'Male', 14, 'A-', '1234567999', 'aaa@gmail.com', '$2y$10$sF0i6sX2wTInabKLsmlUzOS9bcfxACCOmqdwFfNCPXOVHchTBFbFm', '2025-04-07 03:34:16'),
+(2, 'bbb', 'bbb', 'bbb', 'Male', 19, 'O+', '1234567890', 'bbb@gmail.com', '$2y$10$iaHYmMzAWjsSHOz8xUeCfezp6wZYJDMpb47KbTjfZka4Z7T1UKNIi', '2025-04-07 16:14:52');
 
 --
 -- Indexes for dumped tables
